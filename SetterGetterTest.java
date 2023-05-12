4252a21dd3a98b6ae635c0bed73f8d0f9540054a
Gudetama-han <133192502+Gudetama-han@users.noreply.github.com>
2023-05-13 07:25:00 +0900

    Update SetterGetterTest.java

--- SetterGetterTest.java
+++ SetterGetterTest.java
@@ -2,9 +2,9 @@ public class SetterGetterTest {
 	public static void main(String[] args) {
 		MemberDTO member = new MemberDTO();
 			
-		member.SetName("한종호");
+		member.SetName("한호호호");
 		member.SetAge(44);
-		member.SetPhone("010-6399-9899");
+		member.SetPhone("010-1111-1111");
 		
 		System.out.println(member.GetName() + "\t" + member.GetAge() + "\t" + member.GetPhone());
 	}
